5fd5fb1314ca602cf4b401f76448b761cbfc0d8d
Miles <102043640+MilesVorkosigan@users.noreply.github.com>
2024-01-07 18:12:23 +0100

    canvisMirar
    fer canvis mirar

--- Assets/Scripts/Tank/EnemyMovement.cs
+++ Assets/Scripts/Tank/EnemyMovement.cs
@@ -4,21 +4,21 @@ using System.Reflection.Metadata;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine AI;
-
+using UnityEngine.Ai;
 public class EnemyMovement : MonoBehaviour
 {
-    public Transform target;
     UnityEngine.AI.NavMeshAgent agent;
+    Transform target;
     // Start is called before the first frame update
     void Start()
     {
+        target= GameObject.FindGameObjectWihtTag("Player").transform;
         agent= GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(transform.target);
+        agent.SetDestination(target.position);
     }
 }
